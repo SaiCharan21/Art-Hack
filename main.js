db37d2e2b375f4170eae9c7fbf70803e9a56ed7f
Sai Charan <tsaicharan03@gmail.com>
2020-06-13 23:31:27 -0500

    image edit is done

--- main.js
+++ main.js
@@ -1,53 +1,156 @@
-// let myform = document.getElementById('myform');
-// let targetimage = document.getElementById('targetimage');
-// let inputrange = document.querySelectorAll('.slider');
-// myform.addEventListener('submit', function(e){
-//     let urlimage = document.getElementById('onlineurl');
-//     let urlimageval = urlimage.value;
-//     if(urlimageval.length){
-//         targetimage.setAttribute('src',urlimageval);
-//         urlimage.value = '';
-//     }
-    
-//     e.preventDefault();
-//     console.log('done');
-// });
 
-// for(let i=0; i<=inputrange.length-1; i++ ){
-//     inputrange[i].addEventListener('input', editimage);
-// }
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
 
-// function editimage(){
-//     let gs = document.getElementById('gs');
-//     let blur = document.getElementById('blur');
-//     let huerotate = document.getElementById('hue-rotate');
-//     let sepia = document.getElementById('sepia');
+let img = new Image();
+let fileName = '';
 
-//     let gsval = gs.value;
-//     let blurval = blur.value;
-//     let huerotateval = huerotate.value;
-//     let sepiaval = sepia.value;
+const downloadBtn = document.getElementById("download-btn");
+const uploadFile = document.getElementById("upload-file");
+const revertBtn = document.getElementById("revert-btn");
 
-//     targetimage.style.filter = 'grayscale('+gsval+'%) blur('+blurval+'px) hue-rotate('+huerotateval+'deg) sepia('+sepiaval+'%)';
-// }
 
-// let sliderform = document.getElementById('slider-form');
-// sliderform.addEventListener('reset', function(){
-//     sliderform.reset();
-//     setTimeout(function(){
-//         editimage();
-//     },0)
-// })
+uploadFile.addEventListener('change', (e) => {
+    const file = document.getElementById("upload-file").files[0];
+    const reader = new FileReader();
 
-var img;
-window.onload = function(){
-    img = new Foto();
-}
+    if (file) {
+        fileName = file.name;
+        reader.readAsDataURL(file);
+    }
 
-function selectImage(){
-    document.getElementById("foto-file").click();
-}
+    // Add image to canvas
+    reader.addEventListener(
+        "load",
+        () => {
+            // Create image
+            img = new Image();
+            // Set image src
+            img.src = reader.result;
+            // On image load add to canvas
+            img.onload = function () {
+                canvas.width = img.width;
+                canvas.height = img.height;
+                ctx.drawImage(img, 0, 0, img.width, img.height);
+                canvas.removeAttribute("data-caman-id");
+            };
+        },
+        false
+    );
+});
 
-function makeGrayScale(){
-    img.grayscale();
+
+// Filter & Effect Handlers
+document.addEventListener("click", (e) => {
+    if(e.target.classList.contains('filter-btn')){
+        if(e.target.classList.contains('brightness-add')){
+            Caman('#canvas',img,function(){
+                this.brightness(5).render();
+            });
+        } else if (e.target.classList.contains('brightness-remove')){
+            Caman('#canvas', img, function () {
+                this.brightness(-5).render();
+            });
+        }
+        if (e.target.classList.contains('contrast-add')) {
+            Caman('#canvas', img, function () {
+                this.contrast(5).render();
+            });
+        } else if (e.target.classList.contains('contrast-remove')) {
+            Caman('#canvas', img, function () {
+                this.contrast(-5).render();
+            });
+        }
+        if (e.target.classList.contains('saturation-add')) {
+            Caman('#canvas', img, function () {
+                this.saturation(5).render();
+            });
+        } else if (e.target.classList.contains('saturation-remove')) {
+            Caman('#canvas', img, function () {
+                this.saturation(-5).render();
+            });
+        }
+        if (e.target.classList.contains('vibrance-add')) {
+            Caman('#canvas', img, function () {
+                this.vibrance(5).render();
+            });
+        } else if (e.target.classList.contains('vibrance-remove')) {
+            Caman('#canvas', img, function () {
+                this.vibrance(-5).render();
+            });
+        }
+        else if (e.target.classList.contains('vintage-add')) {
+            Caman('#canvas', img, function () {
+                this.vibrance().render();
+            });
+        }
+        else if (e.target.classList.contains('lomo-add')) {
+            Caman('#canvas', img, function () {
+                this.lomo().render();
+            });
+        }
+        else if (e.target.classList.contains('clarity-add')) {
+            Caman('#canvas', img, function () {
+                this.clarity().render();
+            });
+        }
+        else if (e.target.classList.contains('sincity-add')) {
+            Caman('#canvas', img, function () {
+                this.sinCity().render();
+            });
+        }
+        else if (e.target.classList.contains('crossprocess-add')) {
+            Caman('#canvas', img, function () {
+                this.crossProces().render();
+            });
+        }
+        else if (e.target.classList.contains('pinhole-add')) {
+            Caman('#canvas', img, function () {
+                this.pinhole().render();
+            });
+        }
+        else if (e.target.classList.contains('nostalgia-add')) {
+            Caman('#canvas', img, function () {
+                this.nostalgia().render();
+            });
+        }
+        else if (e.target.classList.contains('hermajesty-add')) {
+            Caman('#canvas', img, function () {
+                this.herMajesty().render();
+            });
+        }
+
+    }
+
+});
+
+
+// Revert Filters
+revertBtn.addEventListener("click", e => {
+    Caman("#canvas", img, function () {
+        this.revert();
+    });
+});
+
+// Upload File
+
+
+// Download Event
+downloadBtn.addEventListener("click", () => {
+    const fileExtension = fileName.slice(-4);
+    let newFilename;
+    if (fileExtension === ".jpg" || fileExtension === ".png") {
+        newFilename = fileName.substring(0, fileName.length - 4) + "-edited.jpg";
+    }
+    download(canvas, newFilename);
+});
+
+// Download
+function download(canvas, filename) {
+    let e;
+    const link = document.createElement("a");
+    link.download = filename;
+    link.href = canvas.toDataURL("image/jpeg", 0.8);
+    e = new MouseEvent("click");
+    link.dispatchEvent(e);
 }
